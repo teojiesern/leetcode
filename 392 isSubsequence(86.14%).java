6931a81047bfb319d0f9e1458f2ba9d5c0aa8c72
teojiesern <teojiesern42@gmail.com>
2023-06-05 18:24:25 +0800

    392 isSubsequence 86% minor change

--- 392 isSubsequence(86.14%).java
+++ 392 isSubsequence(86.14%).java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int i=0, j=0;
+        int i = 0, j = 0;
         int sLength = s.length();
         int tLength = t.length();
-        if(sLength == 0) return true;
-        while(i<tLength){
-            if(s.charAt(j) == t.charAt(i)) j++;
-            if(j == sLength) return true;
+        if (sLength == 0)
+            return true;
+        while (i < tLength) {
+            if (s.charAt(j) == t.charAt(i))
+                j++;
+            if (j == sLength)
+                return true;
             // only move forward for s if a match is found
             i++;
         }
-        if(j == sLength) return true;
         return false;
     }
 }
