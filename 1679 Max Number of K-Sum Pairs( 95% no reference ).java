5d6a8acfff5beef3fd7dbff0c86bf080d97d8384
teojiesern <teojiesern42@gmail.com>
2023-06-06 15:44:47 +0800

    done 1679 with no reference and 95% runtime beaten, using two pointers

---
+++ 1679 Max Number of K-Sum Pairs( 95% no reference ).java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int i=0, j=nums.length - 1, numOfOperations = 0;
+        while(i<j){
+            if(nums[i] + nums[j] > k) j--;
+            else if(nums[i] + nums[j] < k) i++;
+            else {
+                numOfOperations++;
+                i++;
+                j--;
+            }        
+        }
+        return numOfOperations;
+    }
+}
